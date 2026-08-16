e9c4b17a56dec38e4e59c2f222d286604735a50d
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-16 18:42:10 -0400

    ref: add loading button

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -45,7 +45,31 @@ async function submitInput() {
                         />
                     </FormError>
                 </div>
-
+                <button
+                    class="flex justify-center bg-blue-500 text-white font-bold py-2 px-4 rounded items-center gap-2 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                    <svg
+                        class="animate-spin h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            stroke-width="4"
+                        ></circle>
+                        <path
+                            class="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                        ></path>
+                    </svg>
+                    Loading in...
+                </button>
                 <button
                     class="rounded-md mb-2 text-white py-2 bg-indigo-500 text-sm font-semibold"
                     @click="submitInput"
