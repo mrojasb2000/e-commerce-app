e8209cddeeb9d1cf25fa17b42910547a6cf2f365
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-16 11:13:31 -0400

    feat: initial commit

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-3xl text-primary">
+    Nuxt 3 +. Tailwind CSS!
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+// tailwindcss.config.js
+export default {
+    // Your Tailwind CSS custom configuration goes here
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#42B883',
+                secondary: '#35495E',
+                neutral: '#F7F9FA'
+            },
+            // 1.5rem * 16 = 24px
+            // 24px / 16 = 1.5rem
+            // 0.5rem = 8 px
+            // 1rem = 16 px
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1.5rem', // horizontal padding
+                    lg: '4rem',
+                    xl: '4rem',
+                    '2xl': '4rem'
+                },
+                screens: {
+                    sm: '576px',
+                    md: '768px',
+                    lg: '992px',
+                    xl: '1200px',
+                    '2xl': '1400px'
+                }
+            }
+        }
+    },
+    plugins: []
+}
