825410a39ae87b2dba68e74224e9dd6bca54426c
Mauricio Rojas <mrojasb2000@gmail.com>
2026-08-16 16:47:22 -0400

    ref: enhance password custom field

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -37,15 +37,14 @@ async function submitInput() {
                             :placeholder="'info@company.com'"
                         />
                     </FormError>
+                    <FormError :errors="v$.password.$errors">
+                        <BaseInput
+                            v-model="registerInputState.password"
+                            :type="'password'"
+                            :placeholder="'Password'"
+                        />
+                    </FormError>
                 </div>
-
-                <input
-                    id=""
-                    name=""
-                    type="password"
-                    placeholder="Password"
-                    class="mb-2 focus:bg-focus-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-0 focus:focus-brand-800"
-                />
 
                 <button
                     class="rounded-md mb-2 text-white py-2 bg-indigo-500 text-sm font-semibold"
